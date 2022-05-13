5708bd384c011d3f867fff67e12cbe46157bda3c
Brandon <brandon.thio@yahoo.com.sg>
2022-05-13 15:55:28 +0800

    Collection was not being found due to typo

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -5,14 +5,15 @@ import { USER_STATE_CHANGE } from "../constants";
 
 export function fetchUser() {
     return ((dispatch) => {
-        firebase.firestore().collection("user")
+        firebase.firestore().collection("users")
             .doc(firebase.auth().currentUser.uid)
             .get()
             .then((snapshot) => {
                 if (snapshot.exists) {
-                    dispatch({type: USER_STATE_CHANGE, currentUser: snapshot.data})
+                    console.log("snapshot exists!",  snapshot.data())
+                    dispatch({type: USER_STATE_CHANGE, currentUser: snapshot.data()})
                 } else {
-                    console.log("user does not exist!")
+                    console.log("User does not exist!")
                 }
             })
     })
